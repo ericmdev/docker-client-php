5b37772fc9311dd55ed6f0d520de2e2118392fe9
Eric Mugerwa <ericmdev@gmail.com>
2016-02-08 20:45:15 +0000

    Add test/DockerClientTestCase.php

---
+++ test/DockerClientTestCase.php
@@ -0,0 +1,12 @@
+<?php
+namespace DockerClient\Test;
+
+/** 
+ * DockerClient test case.
+ *
+ * @package docker-client-php
+ */ 
+abstract class DockerClientTestCase extends PHPUnit_Framework_TestCase
+{
+    
+}
